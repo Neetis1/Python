77193d059252da52056fc8a316eefe19cc173200
Neeti Sharma <sharma1992nee@gmail.com>
2021-08-01 20:46:13 +0530

    basics of condtional statements python

---
+++ 01_conditiona.py
@@ -0,0 +1,43 @@
+# a = int(input("value"))
+# if(a<7):
+#     print("value less than 7")
+# elif(a>7):
+#     print("value is greater than 7")
+# else:
+#     print('equal 7  else')
+    
+# print("yo yo honey singh")
+
+# relational operator
+#== , <= , >= , 
+
+#logical operator 
+# and , or , not
+
+age = 45
+if(age >35 and age < 65):
+    print("nope not possible")
+
+if(age > 35 or age <56):
+    print("ok done")
+
+if(age != 18):
+    print("yo yo honey singh")
+
+#in and is 
+
+a = None
+if(a is None):
+    print("a is none")
+else:
+    print("whatever")
+
+a = [12,4,5,6,7,7,8]
+if(12 in a):
+    print("true")
+else:
+    print("false")
+
+#in is also used in for loops for iteration 
+
+
---
+++ 01_pr_condtionals.py
@@ -0,0 +1,93 @@
+#program 1
+# n1 = int(input())
+# n2 = int(input())
+# n3 = int(input())
+# n4 = int(input())
+# if(n1 > n4):
+#     f1 = n1
+# else:
+#     f1 = n4
+
+# if(n2 > n3):
+#     f2 = n2
+# else:
+#     f2 = n3
+
+# if(f1 > f2):
+#     print(f1, " is greates")
+# else:
+#     print(f2, "is greatest")
+
+# #program 2
+# m1 = int(input())
+# m2 = int(input())
+# m3 = int(input())
+
+# if(m1 < 33 or m2 < 33 or m3 <33):
+#     print("result : FAIL")
+
+# elif((m1+m2+m3/3) < 40 ):
+#     print("result : FAIl")
+# else:
+#     print("result: PASS")
+
+#program 3
+
+# comment = input("enter the comment")
+# spam = False
+# if("make a lot of money" in comment ):
+#     spam = True
+# elif("buy now" in comment):
+#     spam = True 
+# else:
+#     spam = False
+
+# if(spam == True):
+#     print("This is spam")
+# else:
+#     print("not a spam")
+
+#program 4
+# name = input()
+# if(name.__len__() < 10):
+#     print("username is less than 10")
+# else:
+#     print("username is fine")
+
+#program 5
+
+# l1 = ["neeti", "ram", "sita", "laxman", "hanuman"]
+
+# name = input("enter the name")
+
+# if(name in l1):
+#     print("yes in the list")
+# else:
+#     print("Sorry not in the list")
+
+#program 6 
+
+# marks = int(input())
+# if marks >= 90:
+#     grade = "EX"
+# elif marks >=80:
+#     grade = "A"
+# elif marks >=70:
+#     grade = "B"
+# elif marks >=60:
+#     grade = "C"
+# elif marks >=50:
+#     grade = "D"
+# else:
+#     grade = "F"
+
+# print("your grade is ", grade)
+
+#program 7
+
+post = input()
+if("Neeti" in post):
+    print("This post is talking about Neeti Sharma")
+else:
+    print("This post isnt about Neeti")
+
